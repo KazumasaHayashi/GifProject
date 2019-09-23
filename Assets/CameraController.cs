3258d8bcf7d77ddd36a8ac1b2a7774c51fd5481c
KazumsaHayashi <kazumasaice1215@icloud.com>
2019-09-23 23:45:05 +0900

    カメラエフェクト，テレイン

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -1,14 +1,42 @@
-﻿using UnityEngine;
+﻿// using UnityEngine;
+// using System.Collections;
+
+// public class CameraController : MonoBehaviour {
+//     private float timeleft;
+//     public CameraShake shake;
+
+//     void Update () {
+//         //だいたい1秒ごとに処理を行う
+//         timeleft -= Time.deltaTime;
+//         if (timeleft <= 0.0) {
+//             timeleft = 0.0f;
+
+//             shake.Shake(0.25f, 0.05f);
+//         }
+//     }
+// }
+
+using UnityEngine;
+using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
     public CameraShake shake;
+    [SerializeField]
+    private float distance;
+    private float timeFrame;
 
     private void Update()
     {
-        if ( Input.GetKeyDown( KeyCode.Z ) )
+        timeFrame += Time.deltaTime;
+        if ( timeFrame >= distance )
         {
             shake.Shake( 0.25f, 0.05f );
         }
+
+        // if ( Input.GetKeyDown( KeyCode.Z ) )
+        // {
+        //     shake.Shake( 0.25f, 0.05f );
+        // }
     }
 }
---
+++ Assets/TerreinController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using System;
+//using UnityEngine.UI;
+//sing UnityEngine.SceneManagement;
+
+namespace Com.MyCompany.MyGame
+{
+    public class TerreinController : MonoBehaviour
+    {
+        
+        
+        void Start()
+        {
+            
+        }
+
+
+        void Update()
+        {
+            // transformを取得
+            Transform myTransform = this.transform;
+            // 現在の座標からのxyz を1ずつ加算して移動
+            myTransform.Translate (0.0f, 0.0f,  1.0f);
+        }
+
+    }
+}
