5f7edb608ca5f2b430fa819ec58302e793619d2d
KazumsaHayashi <kazumasaice1215@icloud.com>
2019-09-23 15:14:17 +0900

    カメラ設定前

---
+++ Assets/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public CameraShake shake;
+
+    private void Update()
+    {
+        if ( Input.GetKeyDown( KeyCode.Z ) )
+        {
+            shake.Shake( 0.25f, 0.05f );
+        }
+    }
+}
