d41d875ba34dee83cbf56a83da7e66f3acf48d19
KazumsaHayashi <kazumasaice1215@icloud.com>
2019-09-24 18:50:20 +0900

    撮影完了

---
+++ Assets/Gif.cs
@@ -0,0 +1,47 @@
+﻿using Moments;
+using UnityEngine;
+
+public class Gif : MonoBehaviour
+{
+    public Recorder recorder;
+
+    private void Start()
+    {
+        // 撮影を開始します
+        recorder.Record();
+
+        recorder.OnPreProcessingDone = OnProcessingDone;
+        recorder.OnFileSaveProgress = OnFileSaveProgress;
+        recorder.OnFileSaved = OnFileSaved;
+
+        Debug.Log("Gif録画スタート!");
+    }
+
+    // 保存する準備が完了した時に呼び出されます
+    private void OnProcessingDone()
+    {
+    }
+
+    // 保存中に呼び出されます
+    private void OnFileSaveProgress( int id, float percent )
+    {
+    }
+
+    // 保存完了時に呼び出されます
+    private void OnFileSaved( int id, string filepath )
+    {
+        // 保存が完了したら撮影を再開します
+        //recorder.Record();
+        Debug.Log("保存完了！");
+    }
+
+    private void Update()
+    {
+        // スペースキーが押されたら保存します
+        if ( Input.GetKeyDown( KeyCode.Space ) )
+        {
+            recorder.Save();
+            Debug.Log("録画終了！ 保存を待ってね！");
+        }
+    }
+}
